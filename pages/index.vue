f5fc8ecb0314415c2a9500ddb7cd87a034a2c5c8
Jose Ryu Leonesta <ryusu@Joses-MacBook-Pro.local>
2025-03-18 22:22:49 +0700

    removed animated text in index page

--- pages/index.vue
+++ pages/index.vue
@@ -3,30 +3,15 @@
     <section class="min-h-screen flex flex-col justify-center items-center relative">
       <AnimatedBackground />
       <h1 class="text-9xl font-bold">Hi</h1>
-      <p class="text-bold md:text-3xl text-xl typing-animation text-center" v-html="displayText"></p>
+      <p class="text-bold md:text-3xl text-xl typing-animation text-center">
+        My name is Jose Ryu. <br />
+        I'm an active technical lead and <br />
+        software engineer in Indonesia.
+      </p>
     </section>
   </main>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
 import AnimatedBackground from '@/components/AnimatedBackground.vue';
-import { useTypewriter } from '@/composables/useTypewriter';
-
-const texts = [
-  "My name is Jose Ryu...",
-  "<br />Welcome...",
-  " to my portfolio...",
-  "<br />I'm a software engineer...",
-  "<br />This website is still on development...😢"
-];
-
-const { displayText, typeText } = useTypewriter({
-  typingSpeed: 100,
-  pauseBetweenLines: 750
-});
-
-onMounted(async () => {
-  await typeText(texts);
-});
 </script>
