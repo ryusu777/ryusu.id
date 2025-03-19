d94fa5799c8c8ba49bd5bcea20e3db20e4b75a5f
Jose Ryu Leonesta <ryusu@Joses-MacBook-Pro.local>
2025-03-19 09:10:18 +0700

    redesign animated background

--- components/AnimatedBackground.vue
+++ components/AnimatedBackground.vue
@@ -1,43 +1,180 @@
 <template>
-  <div class="absolute inset-0">
-    <svg 
-      viewBox="0 0 200 200" 
+  <div class="fixed w-screen h-screen">
+    <svg
+      id="visual"
+      preserveAspectRatio="none"
+      viewBox="0 0 960 540"
+      class="w-full h-full"
       xmlns="http://www.w3.org/2000/svg"
-      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[800px] h-[800px] opacity-15 blur-[100px]"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      version="1.1"
     >
-      <path 
-        fill="#ff87d7" 
-        :style="`d: path('${currentPath}')`"
-        transform="translate(100 100) scale(1, 1)"
-      />
+      <rect x="0" y="0" width="960" height="540" fill="#141a29"></rect>
+      <defs>
+        <linearGradient id="grad1_0" x1="43.8%" y1="100%" x2="100%" y2="0%">
+          <stop offset="14.444444444444446%" stop-color="#cd52a3" stop-opacity="1"></stop>
+          <stop offset="85.55555555555554%" stop-color="#cd52a3" stop-opacity="1"></stop>
+        </linearGradient>
+      </defs>
+      <defs>
+        <linearGradient id="grad1_1" x1="43.8%" y1="100%" x2="100%" y2="0%">
+          <stop offset="14.444444444444446%" stop-color="#cd52a3" stop-opacity="1"></stop>
+          <stop offset="85.55555555555554%" stop-color="#9c4c96" stop-opacity="1"></stop>
+        </linearGradient>
+      </defs>
+      <defs>
+        <linearGradient id="grad1_2" x1="43.8%" y1="100%" x2="100%" y2="0%">
+          <stop offset="14.444444444444446%" stop-color="#6e4381" stop-opacity="1"></stop>
+          <stop offset="85.55555555555554%" stop-color="#9c4c96" stop-opacity="1"></stop>
+        </linearGradient>
+      </defs>
+      <defs>
+        <linearGradient id="grad1_3" x1="43.8%" y1="100%" x2="100%" y2="0%">
+          <stop offset="14.444444444444446%" stop-color="#6e4381" stop-opacity="1"></stop>
+          <stop offset="85.55555555555554%" stop-color="#473867" stop-opacity="1"></stop>
+        </linearGradient>
+      </defs>
+      <defs>
+        <linearGradient id="grad1_4" x1="43.8%" y1="100%" x2="100%" y2="0%">
+          <stop offset="14.444444444444446%" stop-color="#282a49" stop-opacity="1"></stop>
+          <stop offset="85.55555555555554%" stop-color="#473867" stop-opacity="1"></stop>
+        </linearGradient>
+      </defs>
+      <defs>
+        <linearGradient id="grad1_5" x1="43.8%" y1="100%" x2="100%" y2="0%">
+          <stop offset="14.444444444444446%" stop-color="#282a49" stop-opacity="1"></stop>
+          <stop offset="85.55555555555554%" stop-color="#141a29" stop-opacity="1"></stop>
+        </linearGradient>
+      </defs>
+      <defs>
+        <linearGradient id="grad2_0" x1="0%" y1="100%" x2="56.3%" y2="0%">
+          <stop offset="14.444444444444446%" stop-color="#cd52a3" stop-opacity="1"></stop>
+          <stop offset="85.55555555555554%" stop-color="#cd52a3" stop-opacity="1"></stop>
+        </linearGradient>
+      </defs>
+      <defs>
+        <linearGradient id="grad2_1" x1="0%" y1="100%" x2="56.3%" y2="0%">
+          <stop offset="14.444444444444446%" stop-color="#9c4c96" stop-opacity="1"></stop>
+          <stop offset="85.55555555555554%" stop-color="#cd52a3" stop-opacity="1"></stop>
+        </linearGradient>
+      </defs>
+      <defs>
+        <linearGradient id="grad2_2" x1="0%" y1="100%" x2="56.3%" y2="0%">
+          <stop offset="14.444444444444446%" stop-color="#9c4c96" stop-opacity="1"></stop>
+          <stop offset="85.55555555555554%" stop-color="#6e4381" stop-opacity="1"></stop>
+        </linearGradient>
+      </defs>
+      <defs>
+        <linearGradient id="grad2_3" x1="0%" y1="100%" x2="56.3%" y2="0%">
+          <stop offset="14.444444444444446%" stop-color="#473867" stop-opacity="1"></stop>
+          <stop offset="85.55555555555554%" stop-color="#6e4381" stop-opacity="1"></stop>
+        </linearGradient>
+      </defs>
+      <defs>
+        <linearGradient id="grad2_4" x1="0%" y1="100%" x2="56.3%" y2="0%">
+          <stop offset="14.444444444444446%" stop-color="#473867" stop-opacity="1"></stop>
+          <stop offset="85.55555555555554%" stop-color="#282a49" stop-opacity="1"></stop>
+        </linearGradient>
+      </defs>
+      <defs>
+        <linearGradient id="grad2_5" x1="0%" y1="100%" x2="56.3%" y2="0%">
+          <stop offset="14.444444444444446%" stop-color="#141a29" stop-opacity="1"></stop>
+          <stop offset="85.55555555555554%" stop-color="#282a49" stop-opacity="1"></stop>
+        </linearGradient>
+      </defs>
+      <g transform="translate(960, 540)" class="right-side-blob">
+        <path
+          d="M-540 0C-527.5 -97.3 -515.1 -194.7 -467.7 -270C-420.2 -345.3 -337.8 -398.6 -254 -439.9C-170.2 -481.3 -85.1 -510.6 0 -540L0 0Z"
+          fill="#1d2239"
+        ></path>
+        <path
+          d="M-450 0C-439.6 -81.1 -429.2 -162.2 -389.7 -225C-350.2 -287.8 -281.5 -332.2 -211.7 -366.6C-141.9 -401.1 -70.9 -425.5 0 -450L0 0Z"
+          fill="#363158"
+        ></path>
+        <path
+          d="M-360 0C-351.7 -64.9 -343.4 -129.8 -311.8 -180C-280.1 -230.2 -225.2 -265.7 -169.3 -293.3C-113.5 -320.8 -56.7 -340.4 0 -360L0 0Z"
+          fill="#593e75"
+        ></path>
+        <path
+          d="M-270 0C-263.8 -48.7 -257.5 -97.3 -233.8 -135C-210.1 -172.7 -168.9 -199.3 -127 -220C-85.1 -240.6 -42.6 -255.3 0 -270L0 0Z"
+          fill="#84488c"
+        ></path>
+        <path
+          d="M-180 0C-175.8 -32.4 -171.7 -64.9 -155.9 -90C-140.1 -115.1 -112.6 -132.9 -84.7 -146.6C-56.7 -160.4 -28.4 -170.2 0 -180L0 0Z"
+          fill="#b44f9d"
+        ></path>
+        <path
+          d="M-90 0C-87.9 -16.2 -85.8 -32.4 -77.9 -45C-70 -57.6 -56.3 -66.4 -42.3 -73.3C-28.4 -80.2 -14.2 -85.1 0 -90L0 0Z"
+          fill="#cd52a3"
+        ></path>
+      </g>
+      <g transform="translate(0, 0)" class="left-side-blob">
+        <path
+          d="M540 0C504.2 81.4 468.3 162.8 427.8 247C387.3 331.2 342.1 418.3 270 467.7C197.9 517 99 528.5 0 540L0 0Z"
+          fill="#1d2239"
+        ></path>
+        <path
+          d="M450 0C420.1 67.8 390.3 135.6 356.5 205.8C322.7 276 285.1 348.6 225 389.7C164.9 430.8 82.5 440.4 0 450L0 0Z"
+          fill="#363158"
+        ></path>
+        <path
+          d="M360 0C336.1 54.3 312.2 108.5 285.2 164.7C258.2 220.8 228.1 278.9 180 311.8C131.9 344.6 66 352.3 0 360L0 0Z"
+          fill="#593e75"
+        ></path>
+        <path
+          d="M270 0C252.1 40.7 234.2 81.4 213.9 123.5C193.6 165.6 171 209.2 135 233.8C99 258.5 49.5 264.2 0 270L0 0Z"
+          fill="#84488c"
+        ></path>
+        <path
+          d="M180 0C168.1 27.1 156.1 54.3 142.6 82.3C129.1 110.4 114 139.4 90 155.9C66 172.3 33 176.2 0 180L0 0Z"
+          fill="#b44f9d"
+        ></path>
+        <path
+          d="M90 0C84 13.6 78.1 27.1 71.3 41.2C64.5 55.2 57 69.7 45 77.9C33 86.2 16.5 88.1 0 90L0 0Z"
+          fill="#cd52a3"
+        ></path>
+      </g>
     </svg>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue'
-
-const blobPaths = [
-  "M23.6,-41.2C31.9,-36.1,40.9,-32.4,53.7,-25.8C66.5,-19.2,83.1,-9.6,82.2,-0.5C81.3,8.5,62.8,17,49.3,22.6C35.9,28.1,27.5,30.7,20.2,36.9C12.8,43.2,6.4,53.3,-1.2,55.4C-8.8,57.4,-17.6,51.5,-28,47C-38.5,42.5,-50.6,39.4,-62.7,31.8C-74.8,24.1,-86.9,12.1,-88.2,-0.8C-89.6,-13.6,-80.1,-27.2,-70.7,-39.5C-61.3,-51.9,-52,-62.9,-40.3,-66C-28.5,-69,-14.2,-64,-3.3,-58.3C7.7,-52.6,15.3,-46.2,23.6,-41.2Z",
-  "M34.6,-56.4C47.2,-52.7,61.3,-48.1,64.8,-38.6C68.3,-29.1,61.1,-14.5,59.4,-1C57.6,12.5,61.3,25,58,34.8C54.7,44.6,44.3,51.7,33.5,51.9C22.6,52.1,11.3,45.4,-1.5,48C-14.4,50.7,-28.8,62.7,-41,63.3C-53.2,63.9,-63.3,53.1,-69.2,40.7C-75,28.2,-76.7,14.1,-73,2.2C-69.3,-9.8,-60.1,-19.6,-55,-33.4C-49.9,-47.1,-48.7,-64.8,-40.2,-70.9C-31.7,-77,-15.9,-71.4,-2.4,-67.2C11,-63,22,-60.1,34.6,-56.4Z",
-  "M32.6,-59.9C43.2,-50.4,53.3,-43.5,60.4,-34C67.6,-24.4,71.7,-12.2,75.1,1.9C78.4,16.1,80.9,32.1,71.4,37.6C61.9,43,40.4,37.9,26.5,36.4C12.6,35,6.3,37.1,-3.9,43.9C-14.1,50.7,-28.3,62.1,-39.6,62.1C-50.9,62,-59.4,50.6,-65.9,38.4C-72.4,26.2,-76.9,13.1,-79.1,-1.2C-81.2,-15.5,-80.9,-31.1,-71,-37.3C-61.1,-43.6,-41.4,-40.7,-28,-48.4C-14.5,-56.2,-7.3,-74.7,1.9,-78C11,-81.3,22.1,-69.3,32.6,-59.9Z",
-  "M34.6,-65.5C42,-55.6,43.4,-40.7,52.9,-29C62.3,-17.2,80,-8.6,86.1,3.6C92.3,15.7,86.9,31.4,76.9,42.2C66.8,52.9,52,58.7,38.4,61.8C24.8,64.9,12.4,65.2,-1.6,68C-15.6,70.7,-31.1,75.8,-43.4,71.9C-55.6,68.1,-64.5,55.2,-72.1,41.7C-79.8,28.2,-86.2,14.1,-87.3,-0.7C-88.5,-15.4,-84.3,-30.8,-75.7,-42.8C-67.2,-54.7,-54.2,-63.1,-40.9,-69.6C-27.5,-76.1,-13.8,-80.7,-0.1,-80.5C13.6,-80.3,27.1,-75.4,34.6,-65.5Z"
-]
-
-const currentIndex = ref(0);
-const currentPath = computed(() => blobPaths[currentIndex.value]);
-
-onMounted(() => {
-  setInterval(() => {
-    currentIndex.value = (currentIndex.value + 1) % blobPaths.length;
-  }, 3000);
-});
 </script>
 
 <style scoped>
-svg polyline,
-svg line,
-svg path {
-  transition: 3s linear;
+@keyframes blobAppear {
+  0% {
+    opacity: 0;
+    transform: scale(0.75);
+  }
+  50% {
+    transform: scale(1.0175);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
 }
+
+/* Target each blob with different animation delays */
+g path {
+  animation: blobAppear 1s cubic-bezier(0.25, 0.1, 0.25, 1) forwards;
+  opacity: 0;
+}
+
+/* Left side blobs */
+.right-side-blob path:nth-child(6) { animation-delay: 0ms; }
+.right-side-blob path:nth-child(5) { animation-delay: 50ms; }
+.right-side-blob path:nth-child(4) { animation-delay: 100ms; }
+.right-side-blob path:nth-child(3) { animation-delay: 150ms; }
+.right-side-blob path:nth-child(2) { animation-delay: 200ms; }
+.right-side-blob path:nth-child(1) { animation-delay: 250ms; }
+
+/* Right side blobs */
+.left-side-blob path:nth-child(6) { animation-delay: 0ms; }
+.left-side-blob path:nth-child(5) { animation-delay: 50ms; }
+.left-side-blob path:nth-child(4) { animation-delay: 100ms; }
+.left-side-blob path:nth-child(3) { animation-delay: 150ms; }
+.left-side-blob path:nth-child(2) { animation-delay: 200ms; }
+.left-side-blob path:nth-child(1) { animation-delay: 250ms; }
 </style>
